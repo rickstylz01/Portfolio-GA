3ef5c7e81aaa042c66f0dfc05fde6acd34f4761d
Ricardo Maya <rickmaya@gmail.com>
2023-08-04 08:48:25 -0500

    move quote div styling to mobile view as well

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function openSection(evt, sectionTitle) {
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].classList.remove("active");
-    // tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
